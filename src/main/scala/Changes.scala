bc15c8a2a5b14b49265369b99f90e714c8ff2f05
Robert Thau <rst@ai.mit.edu>
2011-06-29 23:35:23 -0400

    Beginnings of a proper pubsub framework.
    Also, bare beginnings of a proper test suite.

---
+++ src/main/scala/Changes.scala
@@ -0,0 +1,18 @@
+package org.positronic.pubsub
+
+import scala.collection.mutable.HashMap
+
+trait ChangeNotifications {
+  val changeHandlers = new HashMap[ AnyRef, () => Unit ]
+  def onChange( key: AnyRef )( handlerThunk: => Unit ):Unit = {
+    changeHandlers( key ) = (() => handlerThunk)
+  }
+  def stopChangeNotifications( key: AnyRef ):Unit = {
+    changeHandlers.remove( key )
+  }
+  def noteChange = {
+    for ((key, handler) <- changeHandlers) {
+      handler()
+    }
+  }
+}
--- src/test/scala/Specs.scala
+++ src/test/scala/Specs.scala
@@ -1,10 +1,40 @@
-import rst.todo
 import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.Spec
 
+import org.positronic.pubsub.ChangeNotifications
+
 class Specs extends Spec with ShouldMatchers {
-  describe("a spec") {
-    it("should do something") {
+
+  class ChangeDummy extends ChangeNotifications
+
+  describe("change notifications") {
+
+    val cd = new ChangeDummy
+    var callsA = 0
+    var callsB = 0
+
+    it( "should fire one" ) {
+      cd.onChange( "A" ) { callsA += 1 }
+      cd.noteChange
+
+      callsA should be (1)
+      callsB should be (0)
+    }
+
+    it( "should fire two" ) {
+      cd.onChange( "B" ) { callsB += 1 }
+      cd.noteChange
+
+      callsA should be (2)
+      callsB should be (1)
+    }
+
+    it( "should be able to remove notifiers" ) { 
+      cd.stopChangeNotifications( "A" )
+      cd.noteChange
+
+      callsA should be (2)
+      callsB should be (2)
     }
   }
 }

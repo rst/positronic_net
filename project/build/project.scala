dc62c9f7cd6ff99e5b1a5cb503769f772175f73d
Robert Thau <rst@ai.mit.edu>
2011-07-01 17:55:48 -0400

    Make core library and sample app separate subprojects.
    We're still using the Android plugin to compile the core lib, even
    though we don't need its packaging --- so it winds up getting a dummy
    'res' directory, since that's easier right now than figuring out how
    to tell the plugin it doesn't need one.

--- project/build/project.scala
+++ project/build/project.scala
@@ -7,10 +7,13 @@ class Parent(info: ProjectInfo) extends ParentProject(info) {
   override def shouldCheckOutputDirectories = false
   override def updateAction = task { None }
 
-  lazy val main  = project(".", "RstTodo", new MainProject(_))
-  lazy val tests = project("tests",  "tests", new TestProject(_), main)
+  lazy val positronic_net = project(".", "PositronicNet", new LibProject(_))
+  lazy val todo = project("sample_todo_app", "SampleTodo", new SampleProject(_), positronic_net)
+  lazy val todo_tests = project("sample_todo_tests",  "SampleTodoTests", new TestProject(_), positronic_net, todo)
 
-  class MainProject(info: ProjectInfo) extends AndroidProject(info) with Defaults with MarketPublish with TypedResources {
+  class LibProject(info: ProjectInfo) extends AndroidProject(info) with Defaults
+
+  class SampleProject(info: ProjectInfo) extends AndroidProject(info) with Defaults with MarketPublish with TypedResources {
     val keyalias  = "change-me"
     val scalatest = "org.scalatest" % "scalatest" % "1.3" % "test"
   }

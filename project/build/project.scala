42c80480ee19ff73347a6266d03178f7c72477fe
Robert Thau <rst@ai.mit.edu>
2011-07-07 16:40:36 -0400

    Various project definition cleanups.
    Also nuke obsolete tests --- I'm heavily redoing the tested API anyway;
    should restore when done.

--- project/build/project.scala
+++ project/build/project.scala
@@ -7,15 +7,18 @@ class Parent(info: ProjectInfo) extends ParentProject(info) {
   override def shouldCheckOutputDirectories = false
   override def updateAction = task { None }
 
-  lazy val positronic_net = project(".", "PositronicNet", new LibProject(_))
+  lazy val positronic_net = project(".", "PositronicNetLib", new LibProject(_))
   lazy val todo = project("sample_todo_app", "SampleTodo", new SampleProject(_), positronic_net)
   lazy val todo_tests = project("sample_todo_tests",  "SampleTodoTests", new TestProject(_), positronic_net, todo)
 
-  class LibProject(info: ProjectInfo) extends AndroidProject(info) with Defaults
+  class LibProject(info: ProjectInfo) 
+   extends AndroidProject(info) with Defaults with MarketPublish with TypedResources {
+    val keyalias = "change-me"
+    val scalatest = "org.scalatest" % "scalatest" % "1.3" % "test"
+  }
 
   class SampleProject(info: ProjectInfo) extends AndroidProject(info) with Defaults with MarketPublish with TypedResources {
     val keyalias  = "change-me"
-    val scalatest = "org.scalatest" % "scalatest" % "1.3" % "test"
   }
 
   class TestProject(info: ProjectInfo) extends AndroidTestProject(info) with Defaults

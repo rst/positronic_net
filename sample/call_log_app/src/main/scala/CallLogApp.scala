b8882c87ba68f1ddb2a2b8f3a76dc0041d8d6446
Robert Thau <rst@ai.mit.edu>
2011-09-04 23:18:06 -0400

    Call log dumper uses the ORM.

--- sample/call_log_app/src/main/scala/CallLogApp.scala
+++ sample/call_log_app/src/main/scala/CallLogApp.scala
@@ -1,36 +1,44 @@
 package org.positronicnet.sample.call_log
 
 import org.positronicnet.content._
+import org.positronicnet.orm._
 import org.positronicnet.ui._
 
 import android.provider.CallLog.Calls
 import android.util.Log
 
-case class CallLogEntry( callType: Int, number: String, 
-                         cachedName: String, when: Long )
+object Resolver extends PositronicContentResolver( "call_log_app" )
 
-object CallLog
-  extends PositronicContentResolver( "call_log_app" )
+case class CallLogEntry( callType:   Int    = 0, 
+                         number:     String = null, 
+                         cachedName: String = null, 
+                         when:       Long   = 0,
+                         id:         Long   = -1 )
+  extends ManagedRecord( CallLogEntries )
+
+object CallLogEntries
+  extends BaseRecordManager[ CallLogEntry ]( 
+    Resolver( Calls.CONTENT_URI ).order( Calls.DEFAULT_SORT_ORDER ))
 {
-  val src = this( Calls.CONTENT_URI ).order(Calls.DEFAULT_SORT_ORDER)
+  mapField( "callType",   Calls.TYPE )
+  mapField( "number",     Calls.NUMBER )
+  mapField( "cachedName", Calls.CACHED_NAME )
+  mapField( "when",       Calls.DATE )
 
-  def since( when: Long ) = {
-    val qualified_src = src.where( Calls.DATE + ">" + when )
-    for ( c <- qualified_src.select( Calls.TYPE, Calls.NUMBER, 
-                                     Calls.CACHED_NAME, Calls.DATE ))
-    yield CallLogEntry( c.getInt( 0 ), c.getString( 1 ), c.getString( 2 ),
-                        c.getLong( 3 ))
-  }
+  def since( when: Long ) = 
+    this.where( Calls.DATE + "> ?", Array( CvLong( when )))
 }
 
 class CallLogActivity
   extends PositronicActivity
 {
   onCreate {
-    useAppFacility( CallLog )
+    useAppFacility( Resolver )
 
     // Get last ten days worth of calls.  (Roughly.)
-    val stuff = CallLog.since( System.currentTimeMillis - 10*24*60*60*1000 )
-    for ( thing <- stuff ) Log.d( "call_log_app", thing.toString )
+
+    val limit = System.currentTimeMillis - 10*24*60*60*1000
+    for ( entry <- CallLogEntries.since( limit ).fetchOnThisThread ) 
+      Log.d( "call_log_app", entry.toString )
   }
 }

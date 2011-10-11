1bd656f9787734501326a549989997591ad1f17c
Robert Thau <rst@ai.mit.edu>
2011-10-10 21:22:00 -0400

    More thorough package summary for orm.positronicnet.notifications

--- src/main/scala/NotifierPkg.scala
+++ src/main/scala/NotifierPkg.scala
@@ -11,6 +11,35 @@ package org.positronicnet
   * of the result (via a watcher or callback), without the Activity's main
   * UI thread being blocked waiting in the meantime.
   *
+  * The base [[org.positronicnet.notifications.Notifier]] trait specifies a set of actions for querying
+  * the state, and requesting updates when it changes.  Subclasses
+  * (e.g., [[org.positronicnet.orm.RecordManager]]) typically support
+  * other actions which request changes to happen (which may, in turn,
+  * cause watchers to be notified).
+  *
+  * The primary reason for doing things in an actor-style syntax in
+  * Android is to deal with the fairly stringent best-practice requirements
+  * for Android "user interface threads" --- specifically, the requirement
+  * that they shouldn't be waiting for anything else, even local disk I/O
+  * (let alone, say, a network operation).  So, all that stuff needs to
+  * happen on background threads; we use actor style to help keep the
+  * concurrency manageable.
+  *
+  * However, there are also circumstances where concurrency is a nuisance.
+  * For instance, an Android `Service` or `BroadcastReceiver` may want to
+  * be sure that an operation has completed before signalling that it has
+  * finished itself.
+  *
+  * For these situations, you can also request that an action be performed
+  * `onThisThread`, as in
+  *
+  *   `notifier.onThisThread( Fetch{ value => ... } )`
+  *
+  * which performs the `Fetch` and runs the body on the current thread,
+  * forgoing all concurrency.  (Though for this particular case, there's
+  * a special-case `fetchOnThisThread` method which simply fetches the
+  * monitored state and returns it as the value.)
+  *
   * As the name suggests, the notification machinery itself mainly
   * handles the business of notifying other code (typically an `Activity`
   * or `Service`) of current state, and providing updates.

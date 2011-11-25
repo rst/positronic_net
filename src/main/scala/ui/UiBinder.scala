e666dadff4cb150310e2edb3b8c27f8f78a2a426
Robert Thau <rst@ai.mit.edu>
2011-11-24 23:21:31 -0500

    First draft code to link pref-with-key "foo" and reflective property "foo".
    Still more in the way of a feasibility study.  The intent is to support
    bindings of Views to properties (or special-case handling of particular
    kinds of objects, viz. TodoItemView).

---
+++ src/main/scala/ui/UiBinder.scala
@@ -0,0 +1,161 @@
+package org.positronicnet.ui
+
+import android.preference.PreferenceScreen
+import android.preference.Preference
+
+import org.positronicnet.util.ReflectiveProperties
+import org.positronicnet.util.LensFactory
+
+// Feasibility study for binding UI components.
+// May ultimately want to sugar the syntax here.
+//
+// (May also want to ultimately factor out commonalities with 
+// code that walks a View hierarchy.)
+
+object UiBinderImplicits {
+
+  def getPrefs( screen: PreferenceScreen ): Iterable[Preference] = {
+
+    // The following looks hinky, but I can't find another documented
+    // way to get a complete list of the actual prefs!
+
+    val adapter = screen.getRootAdapter
+
+    for (i <- 0 to adapter.getCount) 
+    yield adapter.getItem( i ).asInstanceOf[ Preference ]
+  }
+}
+
+private [ui]
+abstract class UiBinding {
+  def show (widget: Object, props: ReflectiveProperties): Unit
+  def update (widget: Object, props: ReflectiveProperties): ReflectiveProperties
+}
+
+private [ui]
+class BindingManager {
+
+  protected
+  var bindingMap: Map[Class[_], UiBinding] = Map.empty
+
+  protected
+  def noteBinding( klass: Class[_], binder: UiBinding ) =
+    bindingMap += ( klass -> binder )
+
+  private [ui]
+  def findBinder( obj: Object ) = {
+    val klass = obj.getClass
+    bindingMap.get( klass ) match {
+      case Some(binding) => Some(binding)
+      case None =>
+        findBinderForSuperclass( klass.getSuperclass ) match {
+          case Some(binding) => 
+            bindingMap += (klass -> binding)
+            Some( binding )
+          case None =>
+            None
+        }
+    }
+  }
+
+  private
+  def findBinderForSuperclass( klass: Class[_] ): Option[UiBinding] = {
+    var kklass = klass
+    while( kklass != null ) {
+      bindingMap.get( kklass ) match {
+        case Some(binding) => return Some(binding)
+        case None          => kklass = kklass.getSuperclass
+      }
+    }
+    return None
+  }
+}
+
+private [ui]
+class PropertyBinding[ TWidget, TProp : ClassManifest ](
+    readFunc: TWidget => TProp,
+    writeFunc: (TWidget, TProp) => Unit
+  )
+  extends UiBinding
+{
+  // XXX This is ridiculous.  We know which branch of the match will
+  // be taken at declare time; no need for reflection at all here!
+  // At the very least, could have two subclasses with two different
+  // implementations, and let 'PropertyBinder.declare' pick the right
+  // one...
+
+  def propertyName( widget: TWidget ) =
+    widget match {
+      case pref: android.preference.Preference => pref.getKey
+      case view: android.view.View =>
+        throw new RuntimeException( "Not introspecting on resource IDs yet" )
+    }
+
+  val lensFactory = LensFactory.forPropertyType[ TProp ]
+  val propKlass = classManifest[ TProp ].erasure
+
+  def lens (widget: TWidget, props: ReflectiveProperties) = {
+    val propsKlass = props.getClass.asInstanceOf[ Class[Object] ]
+    lensFactory.forProperty( propsKlass, propertyName( widget )) match {
+      case Some( lens ) => lens
+      case None =>
+        throw new RuntimeException( "Could not find property " + 
+                                    propertyName( widget ) + " of type " +
+                                    propKlass.toString + 
+                                    " in class " + propsKlass.toString )
+    }
+  }
+
+  def show (widget: Object, props: ReflectiveProperties) = {
+    val wwidget = widget.asInstanceOf[ TWidget ]
+    writeFunc( wwidget, lens( wwidget, props ).getter( props ) )
+  }
+
+  def update (widget: Object, props: ReflectiveProperties) = {
+    val wwidget = widget.asInstanceOf[ TWidget ]
+    val result = lens( wwidget, props ).setter( props, readFunc( wwidget ))
+    result.asInstanceOf[ ReflectiveProperties ]
+  }
+}
+
+object PropertyBinder extends BindingManager {
+
+  def declare[ TWidget : ClassManifest, TProp : ClassManifest ](
+    readFunc: TWidget => TProp,
+    writeFunc: (TWidget, TProp) => Unit
+  ) =
+    noteBinding( classManifest[ TWidget ].erasure,
+                 new PropertyBinding( readFunc, writeFunc ) )
+}
+
+object UiBinder extends BindingManager {
+
+  private
+  def getBinder( obj: Object ) = {
+    findBinder( obj ) match {
+      case Some (binder) => binder
+      case None => PropertyBinder.findBinder( obj ) match {
+        case Some (binder) => binder
+        case None => 
+          throw new RuntimeException(
+            "No UI binder declared for class " + obj.getClass.toString)
+      }
+    }
+  }  
+
+  def show( props: ReflectiveProperties, prefs: Iterable[Preference] ) = {
+    for (pref <- prefs) 
+      getBinder( pref ).show( pref, props )
+  }
+  
+  def update[T <: ReflectiveProperties]( props: ReflectiveProperties,
+                                         prefs: Iterable[Preference] ): T = {
+    var workingCopy = props
+    for (pref <- prefs)
+      workingCopy = getBinder( pref ).update( pref, props )
+
+    return workingCopy.asInstanceOf[T]
+  }
+}
+
+

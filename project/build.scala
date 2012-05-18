a0c4494e80c03d6d6872ea4cbbf648582fb2cec5
Robert Thau <rst@ai.mit.edu>
2012-05-18 12:16:19 -0400

    Tell ProGuard not to kill Java Serialization hook methods.
    See sbt-android-plugin issue 122.

--- project/build.scala
+++ project/build.scala
@@ -17,7 +17,16 @@ object General {
     TypedResources.settings ++
     AndroidMarketPublish.settings ++ Seq (
       keyalias in Android := "change-me",
-      libraryDependencies += "org.scalatest" %% "scalatest" % "1.6.1" % "test"
+      libraryDependencies += "org.scalatest" %% "scalatest" % "1.6.1" % "test",
+      proguardOption in Android := """
+       -keepclassmembers class * implements java.io.Serializable {
+        private static final java.io.ObjectStreamField[] serialPersistentFields;
+        private void writeObject(java.io.ObjectOutputStream);
+        private void readObject(java.io.ObjectInputStream);
+        java.lang.Object writeReplace();
+        java.lang.Object readResolve();
+       }
+      """
     )
 }
 

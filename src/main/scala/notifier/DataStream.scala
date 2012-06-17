216377f87851cbee6145f243bbcd56f8b9183786
Robert Thau <rst@ai.mit.edu>
2012-06-17 18:04:03 -0400

    Second cut at implementation of the DataStream monad, Duration, etc.
    This version takes into account that a data stream's first value may not
    be available immediately on creation.  It also defines the combinators
    in ways that are, at least, less obviously wrong.  (For example, the
    initial value of the stream returned by "someStream.filter( pred )"
    will always satisfy the predicate, even if the most recent value of the
    underlying stream, at the time of creation, does *not*.)

--- src/main/scala/notifier/DataStream.scala
+++ src/main/scala/notifier/DataStream.scala
@@ -37,22 +37,38 @@ import scala.collection.mutable.HashMap
 
 trait DataStream[T] 
 {
-  /** Most recent value of this DataStream */
-
-  def currentValue: T
-  protected var cachedCurrent = currentValue
-
-  /** Called by the implementation to notify whoever's listening to the
-    * stream that a new value is available.
+  /** A future for the first value of this DataStream, as of the time
+    * of its creation.
+    *
+    * If the implementation only calls `noteNewValue` and never calls
+    * `noteNewFuture` deriving from ExplicitNotificationDataStream
+    * will handle the plumbing...
     */
 
-  protected def notifyNewValue: Unit = {
-    this.cachedCurrent = currentValue
+  protected def initialFuture: Future[T]
+  private [positronicnet] var cachedCurrentFuture = initialFuture
+
+  /** May be called by the implementation to notify whoever's listening to the
+    * stream that a future for a new value is available.
+    */
+
+  protected def noteNewFuture( fut: Future[T] ): Unit = {
+    this.cachedCurrentFuture = fut
     for (listener <- listeners.values)
-      listener( this.cachedCurrent )
+      fut.onSuccess{ listener( _ ) }
   }
 
-  /** Receive this stream's current value, and further values from this
+  /** May be called by the implementation to notify that a particular new
+    * value has arrived.
+    */
+  
+  protected def noteNewValue( value: T ): Unit = {
+    this.cachedCurrentFuture = Future( value )
+    for (listener <- listeners.values)
+      listener( value )
+  }
+
+  /** Receive this stream's next available value, and further values from this
     * stream as they become available, due changes in the underlying
     * resources.
     *
@@ -61,7 +77,7 @@ trait DataStream[T]
     */
 
   def withValues( handler: T => Unit ): Unit = {
-    handler( currentValue )
+    cachedCurrentFuture.onSuccess{ handler( _ ) }
     addListener( new Object, handler )  // dummy tag
   }
 
@@ -112,62 +128,90 @@ trait DataStream[T]
   private[this] var listeners = new HashMap[ AnyRef, T => Unit ]
 }
 
+/** Class representing a condition that starts or stops (e.g., some
+  * activity being active.  In implementation, it is a DataStream of
+  * element type DurationEvent, which in turn has the two distinguished
+  * values DurationStart and DurationStop.
+  *
+  * It is conceded that the naming here sucks.
+  */
+
 trait Duration extends DataStream[DurationEvent]
 
 class DurationEvent
 case object DurationStart extends DurationEvent
 case object DurationStop extends DurationEvent
 
+/** Useful base class (or trait) for DataStreams whose implementations
+  * naturally call only `noteNewValue`, and not `notifyNewValue`.
+  */
+
+trait ExplicitNotificationDataStream[T] extends DataStream[T]
+{
+  final protected override def noteNewFuture( fut: Future[T] ) =
+    throw new RuntimeException( "Called noteNewFuture on a " + 
+                                this.getClass.getName )
+
+  protected val initialFuture = new Future[T]
+  private var haveInitialValue = false
+
+  override protected def noteNewValue( newVal: T ): Unit = {
+    if (! haveInitialValue ) {
+      haveInitialValue = true
+      initialFuture.succeed( newVal )
+    }
+    super.noteNewValue( newVal )
+  }
+}
+
 private class DurationFilteredDataStream[T]( duration: Duration,
                                              underlying: DataStream[T] )
   extends DataStream[T]
 {
-  def currentValue = underlying.currentValue
+  protected def initialFuture = underlying.cachedCurrentFuture
 
-  duration.addListener ( this,  _ match {
+  duration.withValues{  _ match {
 
     case DurationStart =>
-      this.notifyNewValue
-      underlying.addListener( this, dummy => this.notifyNewValue )
+      this.noteNewFuture( underlying.cachedCurrentFuture )
+      underlying.addListener( this, this.noteNewValue( _ ))
 
     case DurationStop =>
       underlying.removeListener( this )
-  })
+  }}
 }
 
 private[notifications] 
 class FlatMapProxyStream[T,V]( mapFunc: T => DataStream[V],
                                underlying: DataStream[T] ) 
-  extends DataStream[V]
+  extends ExplicitNotificationDataStream[V]
 {
-  private[this] var vstream = mapFunc( underlying.currentValue )
+  private[this] var vstream: DataStream[V] = null
 
-  def currentValue = vstream.currentValue
-
-  vstream.addListener( this, dummy => this.notifyNewValue )
-
-  underlying.addListener( this, newVal => {
-    vstream.removeListener( this )
+  underlying.withValues{ newVal => {
+    if (vstream != null) vstream.removeListener( this )
     vstream = mapFunc( newVal )
-    vstream.addListener( this, dummy => this.notifyNewValue)
-  })
+    vstream.cachedCurrentFuture.onSuccess{ this.noteNewValue( _ ) }
+    vstream.addListener( this, this.noteNewValue( _ ))
+  }}
 }
 
 private[notifications] 
 class MapProxyStream[T,V]( mapFunc: T => V,
                            underlying: DataStream[T] ) 
-  extends DataStream[T]
+  extends ExplicitNotificationDataStream[V]
 {
-  def currentValue = underlying.currentValue
-  underlying.addListener( this, dummy => this.notifyNewValue )
+  underlying.withValues{ value => this.noteNewValue( mapFunc( value )) }
 }
 
 private[notifications] 
 class FilterProxyStream[T,V]( pred: T => Boolean,
                               underlying: DataStream[T] ) 
-  extends DataStream[T]
+  extends ExplicitNotificationDataStream[T]
 {
-  def currentValue = underlying.currentValue
-  underlying.addListener( this, 
-    newVal => if (pred(newVal)) this.notifyNewValue)
+  underlying.withValues{ value =>
+    if (pred( value ))
+      this.noteNewValue( value )
+  }
 }
+
